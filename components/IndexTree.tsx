513491867201446cb369ed65df6dc4043867bb90
Zhuxb <ZhuxbClouds@gmail.com>
2024-10-14 00:42:02 +0800

    Refactor IndexTree component to handle decoding of URL path

--- components/IndexTree.tsx
+++ components/IndexTree.tsx
@@ -3,13 +3,16 @@ import type { TreeDataNode, TreeProps } from "antd";
 import { useState } from "react";
 import { getMetaData } from "../utils/meta";
 import { useEffect } from "react";
-
+import { useRouter } from "next/router";
 export default function LinkTree({ treeData }: { treeData: TreeDataNode[] }) {
   const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
   const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
-
+  const router = useRouter();
+  const currentUrl = router.asPath;
   const onSelect: TreeProps["onSelect"] = (selectedKeys, info) => {
     // 如果是叶子节点，就跳转，否则展开/收起
+    // 如果与当前选中的相同，则不处理
+    if (info.node.key == decodeURI(currentUrl).replace("/posts/", "")) return;
     if (info.node.children?.length) {
       setExpandedKeys(
         expandedKeys.includes(info.node.key)
